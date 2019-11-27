4492c95b358a37d517036b898c559da601820d52
Paulo Ragonha <paulo@ragonha.me>
2019-11-27 20:06:59 +0100

    Setup a loader and its dependency from the UI 📦

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import { generateProgram } from '@underverk/compiler'
+
+export default function(jsonSource: string) {
+  return generateProgram(JSON.parse(jsonSource))
+}
