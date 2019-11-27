96955bdb8602508470ba7b9c013d228c1ef00dca
Paulo Ragonha <paulo@ragonha.me>
2019-11-27 20:55:52 +0100

    Simple server that allows getting and updating the underverk example 👾

--- examples/app-consuming-underverk/src/index.jsx
+++ examples/app-consuming-underverk/src/index.jsx
@@ -7,3 +7,18 @@ import App from './testProgram.underverk.json'
 // console.log('🧱', testProgram)
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+const yay = async () => {
+  const read = await fetch('./example.json')
+  const myJson = await read.json()
+
+  await fetch('./example.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(myJson),
+  })
+}
+
+yay()
